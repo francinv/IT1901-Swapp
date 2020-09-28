429a8e10b5468e727d944915d77ca3ec64ceddfc
Gaute Brandser <gautbra@stud.ntnu.no>
2020-09-28 11:03:14 +0000

    ferdigstilt appnavn

--- swapp/src/main/java/swapp/ui/Register.java
+++ swapp/src/main/java/swapp/ui/Register.java
@@ -6,7 +6,7 @@ import javafx.scene.Parent;
 import javafx.scene.Scene;
 import javafx.stage.Stage;
 
-	public class App extends Application {
+	public class Register extends Application {
 
 	  @Override
 	  public void start(final Stage primaryStage) throws Exception {
