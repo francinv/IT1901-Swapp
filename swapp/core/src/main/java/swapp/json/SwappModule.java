b191480247574ad66df57e4b02bb3001da3403ed
Francin Anoj Vincent <francinv@stud.ntnu.no>
2020-10-09 08:18:04 +0000

    Endret module for json

--- swapp/core/src/main/java/swapp/json/SwappModule.java
+++ swapp/core/src/main/java/swapp/json/SwappModule.java
@@ -7,6 +7,7 @@ import com.fasterxml.jackson.databind.module.SimpleSerializers;
 
 import swapp.core.Swapp;
 import swapp.core.User;
+import swapp.core.AdList;
 
 public class SwappModule extends Module {
 
@@ -15,9 +16,11 @@ public class SwappModule extends Module {
 	
 	public SwappModule() {
 		serializers.addSerializer(Swapp.class, new SwappSerializer());
-		serializers.addSerializer(User.class, new UserSerializer());
+        serializers.addSerializer(User.class, new UserSerializer());
+        serializers.addSerializer(AdList.class, new AdListSerializer());
 		deserializers.addDeserializer(Swapp.class, new SwappDeserializer());
-		deserializers.addDeserializer(User.class, new UserDeserializer());
+        deserializers.addDeserializer(User.class, new UserDeserializer());
+        deserializers.addDeserializer(AdList.class, new AdListDeserializer());
 	}
 	
 	@Override
