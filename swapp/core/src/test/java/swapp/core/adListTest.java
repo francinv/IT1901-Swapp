fff344e1b5c63a9c47d460507b437f1df027dff8
henrikya <henrikya@stud.ntnu.no>
2020-10-11 13:08:46 +0200

    litt mer tester

--- swapp/core/src/test/java/swapp/core/adListTest.java
+++ swapp/core/src/test/java/swapp/core/adListTest.java
@@ -1,47 +1,44 @@
 package swapp.core;
 
+import org.junit.jupiter.api.BeforeAll;
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 
 public class adListTest {
   AdList adList;
 
-  @Test
+  @BeforeEach
   public void setUp() throws Exception{
     System.out.println("Test coverage report can be found in target/site/jacoco/index.html");
 
     this.adList = new AdList();
-    createAd();
-    removeAd();
   }
 
   @Test
-  public void createAd() {
-    try {
-      int NumberofAds = adList.getNumberOfAds();
+  public void testCreateAd() {
+      System.out.println("HEHEHE ");
+      int numberOfAds = adList.getNumberOfAds();
+
       adList.createAndAdd("nepe", "lars", "Godt brukt");
       adList.createAndAdd("koll", "ødipus", "brukt");
       adList.createAndAdd("kål", "Pante-per", "Pen");
-      assert adList.getNumberOfAds() == NumberofAds+3;
-      assert adList.getAd(NumberofAds).getAuthor().equals("lars");
-      assert adList.getAd(NumberofAds).getTitle().equals("nepe");
-      assert adList.getAd(NumberofAds).getTextBody().equals("Godt brukt");
-      assert adList.getAd(NumberofAds+2).getAuthor().equals("Pante-per");
-    }
-    catch (Exception e) {
-    }
+      assert adList.getNumberOfAds() == numberOfAds+3;
+
+      assert adList.getAd(numberOfAds).getAuthor().equals("lars");
+      assert adList.getAd(numberOfAds).getTitle().equals("nepe");
+      assert adList.getAd(numberOfAds).getTextBody().equals("Godt brukt");
+      assert adList.getAd(numberOfAds+2).getAuthor().equals("Pante-per");
+
   }
 
   @Test
-  public void removeAd() {
-    try {
+  public void testRemoveAd() {
+
       int NumberofAds = adList.getNumberOfAds();
       assert adList.getNumberOfAds() == NumberofAds;
       Ad ad = adList.getAd(0);
       adList.archiveAd(ad);
       assert adList.getNumberOfAds() == NumberofAds-1;
 
-      //
-    } catch (Exception e) {
-    }
   }
 }
